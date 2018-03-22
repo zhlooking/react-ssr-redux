2c8ce94dbfe4c8416eca4f2e48b37d5912b4f4cd
zhanghao <zhanghao@zcool.com.cn>
2018-03-22 17:27:17 +0800

    FIX: wrong webpack pack entry file path bug

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,7 +5,7 @@ const autoprefixer = require("autoprefixer");
 const path = require('path');
 
 module.exports = {
-  entry: 'server.js',
+  entry: './server.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'server.js',
